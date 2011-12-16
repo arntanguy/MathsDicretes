783cbc3e50cb28a270646994b6ee9387f1ecb3d5
Kauv <Kauv@x104>
2011-12-16 17:48:12 +0100

    Longueur fait

--- src/exo1/TestCk.java
+++ src/exo1/TestCk.java
@@ -8,22 +8,20 @@ import java.util.Scanner;
  * @version 1.0
  */
 public class TestCk {
- 
-  
-    /**
-     * Dessin de Ck
-     *
-     */
-    public static void main(String[] args) {
-	System.out.println(" dessin de C recursifs");
-        System.out.println("saisir un entier, pour la  profondeur de recursion?");
-        Scanner sc = new Scanner(System.in);
-	int profondeur = sc.nextInt();
 
-	CkSol t = new CkSol(profondeur); 
 
-  }
- 
+	/**
+	 * Dessin de Ck
+	 *
+	 */
+	public static void main(String[] args) {
+		System.out.println(" dessin de C recursifs");
+		System.out.println("saisir un entier, pour la  profondeur de recursion?");
+		Scanner sc = new Scanner(System.in);
+		int profondeur = sc.nextInt();
+
+		CkSol t = new CkSol(profondeur); 
+	}
+
 }
-      
-  
+

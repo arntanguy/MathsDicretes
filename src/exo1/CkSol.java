a40279fce80f54d0b0b6356dc50c09fcccb6487a
TANGUY Arnaud <arn.tanguy@gmail.com>
2011-12-16 10:39:39 +0100

    Exo 1 : calcul correct des triangles. Pb: trace tous les traits
    Les coordonnées du triangle sont calculées, et la récursion est bien
    effectué. Le seul problème est que tous les traits sont tracés, au lieu
    de juste ceux nécessaires. A corriger

--- src/exo1/CkSol.java
+++ src/exo1/CkSol.java
@@ -7,8 +7,6 @@ public class CkSol extends Ck {
 
 	public CkSol(int profondeur) {
 		super(profondeur);
-	    setBackground (Color.white); // Le fond est blanc
-
 	}
 	
     /** drawCk 
@@ -24,6 +22,31 @@ public class CkSol extends Ck {
      * Postcondition : Ck est dessiné
      **/ 
     public void drawCk(int x1, int y1, int x2, int y2, int k ,  Graphics drawingArea)  {
+    	if(k>0) {
+    	// Longeur de la base
+    	double lb = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+    	// Longeur du côté (2lc^2 = lb^2 par pythagore)
+    	double lc = lb/Math.sqrt(2);
+    	// hauteur du rectangle
+    	double h = (lb/2)*Math.tan(Math.PI/4);
+    	double xdirecteur = (y2-y1);
+    	double ydirecteur = -(x2-x1);
+    	double coeffNormatif = 1/Math.sqrt(xdirecteur*xdirecteur + ydirecteur*ydirecteur);
+
+    	double xmilieu = (x1+x2)/2;
+    	double ymilieu = (y1+y2)/2;
+    	int xsommet = (int)(xmilieu + h*coeffNormatif * xdirecteur);
+    	int ysommet = (int)(ymilieu + h*coeffNormatif * ydirecteur);
     	
+        drawingArea.drawLine(x1, y1, xsommet, ysommet);
+    	drawingArea.drawLine(xsommet, ysommet, x2, y2);
+    	drawCk(xsommet, ysommet, x2, y2, k-1, drawingArea);
+    	drawCk(x1, y1, xsommet, ysommet, k-1, drawingArea);
+
+    	System.out.println("x1: "+x1+"\tx2: "+x2+"\txs: "+xsommet+"\n"+
+    			"y1: "+y1+"\ty2: "+y2+"\tysomment: "+ysommet+"\n"+
+    			"h: "+h);
+    	}
     }
+    
 }

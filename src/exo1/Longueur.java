0e0b8f44c5bb587f84ead76d99e65e192130ac8b
Kauv <Kauv@x104>
2011-12-16 18:15:30 +0100

    t

--- src/exo1/Longueur.java
+++ src/exo1/Longueur.java
@@ -4,7 +4,7 @@ public class Longueur {
 
 	private static double calculLong (double base, int nbFois, int puis) {
 		if (nbFois == 0) return base * puis;
-		return calculLong(base / Math.sqrt(2.0), nbFois - 1, 2*puis);
+		return calculLong (base / Math.sqrt(2.0), nbFois - 1, 2*puis);
 	}
 	
 	public static double Long (double base, int nbFois) {
